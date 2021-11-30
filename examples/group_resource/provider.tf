e173b64790bbc8ef23a269c395edce64fd293767
Restyled.io <commits@restyled.io>
2021-11-30 16:16:28 +0000

    Restyled by terraform

--- examples/group_resource/provider.tf
+++ examples/group_resource/provider.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     groupme = {
       # this provider isn't in a registry yet, so I have to copy build into local terraform.d/
-      source = "local/providers/groupme"
+      source  = "local/providers/groupme"
       version = "1.0.0"
     }
 
     random = {
-      source = "registry.terraform.io/hashicorp/random"
+      source  = "registry.terraform.io/hashicorp/random"
       version = "3.1.0"
     }
   }
--- examples/group_resource/variables.tf
+++ examples/group_resource/variables.tf
@@ -1,9 +1,9 @@
 variable groupme_token {
-  type = string
+  type        = string
   description = "api key to call groupme apis"
-  sensitive = true
+  sensitive   = true
   validation {
-    condition = length(var.groupme_token) > 8
+    condition     = length(var.groupme_token) > 8
     error_message = "Must provide a token!"
   }
 }
